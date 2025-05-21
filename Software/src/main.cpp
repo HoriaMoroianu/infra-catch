8623059d454b7be58a2842eb77d90fd417eaf8bb
Horia Moroianu <moroianu.horiav@gmail.com>
2025-05-21 11:15:51 +0300

    Added IR and LCD libraries and demo code
    Signed-off-by: Horia Moroianu <moroianu.horiav@gmail.com>

--- Software/src/main.cpp
+++ Software/src/main.cpp
@@ -1,14 +1,57 @@
 #include <avr/io.h>
 #include <Arduino.h>
+#include <IRRemote.hpp>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,  16, 2);
+
+byte heart[8] = {
+  0b00000,
+  0b01010,
+  0b11111,
+  0b11111,
+  0b11111,
+  0b01110,
+  0b00100,
+  0b00000
+};
+
+void LCD_demo() {
+  lcd.setCursor(0, 0);
+  lcd.print("Lives: ");
+  lcd.write(0);
+  lcd.write(0);
+  lcd.write(0);
+  lcd.write(0);
+  lcd.write(0);
+
+  lcd.setCursor(0, 1);
+  lcd.print("Score: 0");
+}
 
 int main(void) {
+  // Initialize the Arduino library
   init();
-  Serial.begin(9600);
-  Serial.println("Hello, World!");
 
+  // LCD setup
+  lcd.init();
+  lcd.backlight();
+  lcd.createChar(0, heart);
+  LCD_demo();
+  
+  // IR receiver setup
+  IrReceiver.begin(2, ENABLE_LED_FEEDBACK);
+
+  // Enable serial communication
+  Serial.begin(9600);
+  Serial.println("Ready to receive IR signals");
+  
   while (1) {
-      delay(1000);
-      Serial.println("Running...");
+    if (IrReceiver.decode()) {
+      Serial.print("Received: 0x");
+      Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
+      IrReceiver.resume();
+    }
   }
   return 0;
 }
