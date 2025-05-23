ce804651c780fb6e9093ec3ea45c3fbe61381b63
Horia Moroianu <moroianu.horiav@gmail.com>
2025-05-23 18:35:16 +0300

    Added timer configuration files
    Signed-off-by: Horia Moroianu <moroianu.horiav@gmail.com>

---
+++ Software/include/timers.h
@@ -0,0 +1,18 @@
+#ifndef TIMERS_H
+#define TIMERS_H
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+/**
+ * This function sets up timer1 in CTC mode with a prescaler of 1024 for LED 
+ * delay intervals and enables the timer compare interrupt.
+ */
+void initLedTimer(void);
+
+/**
+ * Starts the LED timer with the specified delay.
+ */
+void startLedTimer(void);
+
+#endif // TIMERS_H
---
+++ Software/src/timers.cpp
@@ -0,0 +1,18 @@
+#include "timers.h"
+
+uint16_t led_delay = 20000; // 1.28 seconds at 16MHz with 1024 prescaler
+
+void initLedTimer(void) {
+  TCCR1A = 0; // Clear control register A
+  TCCR1B = 0; // Clear control register B
+
+  TCCR1B |= (1 << WGM12); // Set CTC mode
+
+  OCR1A = led_delay; // Set compare value for 16MHz with 1024 prescaler
+  TIMSK1 |= (1 << OCIE1A); // Enable timer compare interrupt
+}
+
+void startLedTimer(void) {
+  TCNT1 = (uint16_t)0; // Reset timer counter
+  TCCR1B |= (1 << CS12) | (1 << CS10); // Start timer with prescaler of 1024
+}
