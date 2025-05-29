59fe2c8e5ff382991b84883ded90a5fbaebd050a
Horia Moroianu <moroianu.horiav@gmail.com>
2025-05-29 17:38:55 +0300

    Implemented buzzer functionality using timer0
    Signed-off-by: Horia Moroianu <moroianu.horiav@gmail.com>

--- Software/include/timers.h
+++ Software/include/timers.h
@@ -20,4 +20,22 @@ void startLedTimer(void);
  */
 void stopLedTimer(void);
 
+/**
+ * Calculates the appropriate OCR value and prescaler for a given frequency.
+ */
+void timer_freq_prescale(uint32_t freq, uint8_t *a_ocr, uint8_t *a_prescaler);
+
+/**
+ * Initializes the buzzer by setting up timer0 in CTC mode.
+ */
+void initBuzz();
+
+/**
+ * Buzzes the buzzer at a specified frequency for a specified duration.
+ * 
+ * @param a_freq Frequency in Hz.
+ * @param a_duration Duration in milliseconds.
+ */
+void buzz(uint16_t a_freq, uint16_t a_duration);
+
 #endif // TIMERS_H
--- Software/src/timers.cpp
+++ Software/src/timers.cpp
@@ -1,6 +1,20 @@
 #include "timers.h"
 
+#include <Arduino.h>
+
 uint16_t led_delay = 31250; // 2 seconds at 16MHz with 1024 prescaler
+volatile uint32_t duration = 0; // Duration for the buzzer
+
+ISR(TIMER0_COMPA_vect) {
+	if (duration != 0) {
+    duration--;
+	} else {
+    TIMSK0 &= ~(1 << OCIE0A); // Disable the timer compare interrupt
+		TCCR0B &= ~((1 << CS02) | (1 << CS01) | (1 << CS00)); // Disable clock
+		PORTD &= ~(1 << PD6); // Set the port state to low
+    TCCR0A &= ~(1 << COM0A0); // Disable toggle on compare match
+	}
+}
 
 void initLedTimer(void) {
   TCCR1A = 0; // Clear control register A
@@ -19,4 +33,55 @@ void startLedTimer(void) {
 
 void stopLedTimer(void) {
   TCCR1B &= ~((1 << CS12) | (1 << CS10)); // Stop the timer
+}
+
+void timer_freq_prescale(uint32_t freq, uint8_t *a_ocr, uint8_t *a_prescaler) {
+  // Prescaler values for 1, 8, 64, 256, 1024
+  uint8_t pow[] = { 0, 3, 6, 8, 10, 0 };
+
+  uint16_t ocr = 0;
+  uint8_t prescaler = 0;
+
+  // Find the best prescaler and OCR value for the given frequency
+  do {
+    ocr = (uint16_t) (F_CPU / ((freq << 1) * (1 << pow[prescaler])));
+    prescaler++;
+  } while ((ocr > 255) && (pow[prescaler]));
+
+  ocr--;
+  if (ocr > 255) {
+    ocr = 255;
+  }
+
+  *a_ocr = ocr & 0xff;
+  *a_prescaler = prescaler;
+}
+
+void initBuzz() {
+  TCCR0A = 0; // Clear control register A
+  TCCR0B = 0; // Clear control register B
+  TCCR0A |= (1 << WGM01);  // Set CTC mode
+
+  DDRD |= (1 << PD6);   // Set OC0A pin (PD6) as output
+  PORTD &= ~(1 << PD6); // Ensure OC0A pin is low initially
+}
+
+void buzz(uint16_t a_freq, uint16_t a_duration) {
+  uint8_t prescaler = 0;
+  uint8_t ocr = 0;
+  duration = 0; // Reset duration
+
+  // Calculate the OCR value and prescaler for the given frequency
+  timer_freq_prescale(a_freq, &ocr, &prescaler);
+
+  // Set the OCR0A value and reset the timer counter
+  TCNT0 = 0;
+  OCR0A = ocr;
+  TCCR0A |= (1 << COM0A0); // Enable toggle on compare match
+  
+  // Calculate the amount of cycles
+  duration = ((uint32_t)a_freq * a_duration / 500);
+
+  TIMSK0 |= (1 << OCIE0A); // Enable timer compare interrupt
+  TCCR0B = (prescaler & 0b111); // Set prescaler bits in TCCR0B
 }
